8c6ad20f5b0d842c56a2b562cf33e7b8b3ff86f6
Ruan Arcega <ruanarcega@gmail.com>
2019-11-10 22:10:25 -0300

    feat: configure index

--- src/index.js
+++ src/index.js
@@ -4,9 +4,10 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
+ReactDOM.render(
+    <div>
+        <h1>Calculator</h1>
+        <App />
+    </div>
+, document.getElementById('root'));
 serviceWorker.unregister();
